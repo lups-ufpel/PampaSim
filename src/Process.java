b1ee6f253bebff8890e7f47b4ce3a8ec7c93da56
joao <jemplay00@gmail.com>
2023-07-01 21:37:33 -0300

    Classe PCB renomeada para Processo, remoovido o atributo priority (por enquanto)

---
+++ src/Process.java
@@ -0,0 +1,32 @@
+
+public final class Process {
+	private int pid;
+	private int numberOfInstructions;
+	private int priority;
+	//private int arrivalTime;
+	public int programCounter;
+	private ProcessState state;
+
+	public Process(int pid, int priority, ProcessState state, int numberOfInstructions) {
+		this.pid = pid;
+		this.state = state;
+		this.priority = priority;
+		this.programCounter = 0;
+		this.numberOfInstructions = numberOfInstructions;
+	}
+
+	public int getPid() {
+		return this.pid;
+	}
+
+	public ProcessState getState() {
+		return this.state;
+	}
+
+	public void setState(ProcessState state) {
+		this.state = state;
+	}
+	public int getNumberOfInstructions(){
+		return this.numberOfInstructions;
+	}
+}
