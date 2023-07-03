e79c811c49b1896992d393a179927d3dfd42f44d
joao <jemplay00@gmail.com>
2023-07-03 12:29:13 -0300

    Refatoração da classe VirtualMachine em 3 métodos principais "init", "run" & "clean"

--- src/simuladorso/VirtualMachine.java
+++ src/simuladorso/VirtualMachine.java
@@ -1,30 +1,60 @@
 package simuladorso;
 
 public class VirtualMachine {
-    public static void main(String[] args) {
-        Processor processor = Processor.getInstance();
-        RoundRobinScheduler sched = new RoundRobinScheduler(2);
-        Kernel kernel = Kernel.getInstance();
-        Process process;
-        //Creating 3 process for testing
-        kernel.fork(sched);
-        kernel.fork(sched);
-        kernel.fork(sched);
-        long startTime = System.currentTimeMillis();
+
+    // atributos provisórios até implementar I/O com usuário
+    private static final int TIME_SLICE =3;
+    private static final int NUM_PROCESSES = 3;
+    
+    long startTime;
+    long endTime;
+    //----------------------------
+    Processor processor;
+    Kernel kernel;
+    RoundRobinScheduler scheduler;
+    private void initialize(){
+        processor = Processor.getInstance();
+        kernel = Kernel.getInstance();
+        scheduler = new RoundRobinScheduler(TIME_SLICE);
+        createFixedNumberProcesses();
+    }
+    private void runSimulation(){
+
+        Process processPlaceHolder;
         while(true) {
-            process = kernel.scheduleProcess(sched);
-            System.out.println("Processo PID: " + process.getPid() + " pronto para executar");
-            for (int i = 1,quantum = sched.getTimeSlice(); i <= quantum; i++) {
-                       if(!processor.cycle(process))break;
+            processPlaceHolder = kernel.scheduleProcess(scheduler);
+            System.out.println("Processo PID: " + processPlaceHolder.getPid() + " pronto para executar");
+            
+            //Um processo executa uma quantidade fixa de ciclos de cpu
+            //baseado na fatia de tempo(quantum) alocada pelo escalonador
+            for (int i = 1,quantum = scheduler.getTimeSlice(); i <= quantum; i++) {
+                       if(!processor.cycle(processPlaceHolder))break;
                     }
 
-            kernel.preemptProcess(sched);
+            kernel.preemptProcess(scheduler);
             
-            if (sched.verifyAllQueues())break;
+            //Se todos os procesos encontram-se no estado TERMINADO
+            // a simulação é encerrada
+            if (scheduler.verifyAllQueues())break;
          }
-        long endTime = System.currentTimeMillis();
+    }
+    private void cleanup(){
+        endTime = System.currentTimeMillis();
+        System.out.println("Finalizando Execução do Simulador.");
         double duration = (endTime - startTime) / 1000; // Duração em segundos
         System.out.println("Tempo de execução: " + duration + " segundos");
-        System.out.println("Finalizando Execução do Simulador.");
+    }
+    private void createFixedNumberProcesses(){
+        for(int i=0;i<NUM_PROCESSES;i++){
+            kernel.fork(scheduler);
+        }
+    }
+
+    public static void main(String[] args) {
+        VirtualMachine machine = new VirtualMachine();
+        machine.initialize();
+        machine.startTime = System.currentTimeMillis();
+        machine.runSimulation();
+        machine.cleanup();
     }
 }
