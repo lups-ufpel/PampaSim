3b42e0992ae2cf5cfd4116b5ff811052b8c9dc87
Gabriel Leite Bessa <gabrielleitebessa@gmail.com>
2023-07-29 19:36:53 -0300

    Adicionando uma interface melhorada

--- app/src/main/java/simuladorso/GUI/App.java
+++ app/src/main/java/simuladorso/GUI/App.java
@@ -24,20 +24,20 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/layout3.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/PrimeiraInterface.fxml"));
         Parent root = fxmlLoader.load();
         Rectangle2D screenInfo = Screen.getPrimary().getBounds();
         double screenWidth = screenInfo.getWidth();
         double screenHeigth = screenInfo.getHeight();
 
-        Button createNewButton = (Button) fxmlLoader.getNamespace().get("createNew");
+        /*Button createNewButton = (Button) fxmlLoader.getNamespace().get("createNew");
         createNewButton.setOnAction(e -> Functions.mudarTexto(createNewButton));
-
+        */
         MenuItem sairMenu = (MenuItem) fxmlLoader.getNamespace().get("sairMenu");
         sairMenu.setOnAction(e -> Functions.sair());
-
+        
         Scene tela = new Scene(root, screenWidth, screenHeigth-70);
-
+        
         primaryStage.setTitle("Interface do vinizão boladão");
         primaryStage.setScene(tela);
         primaryStage.show();
