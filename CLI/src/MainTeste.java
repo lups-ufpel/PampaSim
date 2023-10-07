acf2f99140db1c0713c9eb9e115ba73a7abbeeb4
joao <jemplay00@gmail.com>
2023-10-07 16:08:19 -0300

    Adicionado Escalonador com política SJF

--- CLI/src/MainTeste.java
+++ CLI/src/MainTeste.java
@@ -4,13 +4,14 @@ import VirtualMachine.*;
 import VirtualMachine.Processor.Core;
 import Os.SchedulerFCFS;
 import Os.SchedulerPriority;
+import Os.SchedulerSJF;
 import Os.Os;
 import Os.Scheduler;
 import Os.Process;
 
 public class MainTeste {
 
-    static final Process.Type proc_type = Process.Type.SIMPLE_WITH_PRIORITY;
+    static final Process.Type proc_type = Process.Type.SIMPLE;
     static final int numCores = 2;
     
     //Componentes principais
@@ -20,7 +21,8 @@ public class MainTeste {
     static  Vm <? extends Core> vm;
     public static void main(String[] args){
         //fcfsTest();
-        priorityTest();
+        //priorityTest();
+        SJFTest();
     }
     public static void fcfsTest(){
         mediator = new MediatorDefault();
@@ -56,5 +58,21 @@ public class MainTeste {
         mediator.registerComponent(Mediator.Component.OS, kernel);
         mediator.registerComponent(Mediator.Component.SCHEDULER, scheduler);
         mediator.registerComponent(Mediator.Component.VM, vm);
+    }
+    public static void SJFTest(){
+        mediator = new MediatorDefault();
+        kernel = new Os();
+        scheduler = new SchedulerSJF(kernel.getNewList(), 
+                                                kernel.getReadyList(), 
+                                                kernel.getWaitingList(), 
+                                                kernel.getTerminatedList(),
+                                                numCores, mediator);
+        vm = new VmSimple(numCores, mediator);
+        registerComponent();
+        mediator.invoke(Mediator.Action.KERNEL_NEW_PROCESS, new Object[]{proc_type, 10});
+        mediator.invoke(Mediator.Action.KERNEL_NEW_PROCESS, new Object[]{proc_type, 6});
+        mediator.invoke(Mediator.Action.KERNEL_NEW_PROCESS, new Object[]{proc_type, 4});
+        mediator.invoke(Mediator.Action.KERNEL_NEW_PROCESS, new Object[]{proc_type, 2});
+        vm.run();                                   
     }
 }
