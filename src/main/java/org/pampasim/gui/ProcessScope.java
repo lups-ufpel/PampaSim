5897f1bb14874dfb73bfb5d96e561c91e398a635
jansoares <jansoares@inf.ufpel.edu.br>
2024-06-24 00:28:00 -0300

    Refactoring Dialog's Spinners code.

--- src/main/java/org/pampasim/gui/ProcessScope.java
+++ src/main/java/org/pampasim/gui/ProcessScope.java
@@ -5,9 +5,10 @@ import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
 
 public class ProcessScope implements Scope {
-    private final Property<Integer> burstProperty = new SimpleObjectProperty<>(1);
-    private final Property<Integer> priorityProperty = new SimpleObjectProperty<>(1);
-    private final Property<Integer> durationProperty = new SimpleObjectProperty<>(1);
+    private static final int INITIAL_VALUE = 1;
+    private final Property<Integer> burstProperty = new SimpleObjectProperty<>(INITIAL_VALUE);
+    private final Property<Integer> priorityProperty = new SimpleObjectProperty<>(INITIAL_VALUE);
+    private final Property<Integer> durationProperty = new SimpleObjectProperty<>(INITIAL_VALUE);
 
     public Property<Integer> getBurstProperty() {
         return burstProperty;
