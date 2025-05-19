ffc2005180dc31343031155ebda23c6f11a15105
IgorDutra <igorgomesdutra@gmail.com>
2025-05-19 12:22:08 -0300

    small steps in implementing the PageTableManager

--- memory/src/main/java/org/pampasim/memory/entity/PageTableManager.java
+++ memory/src/main/java/org/pampasim/memory/entity/PageTableManager.java
@@ -1,11 +1,14 @@
 package org.pampasim.memory.entity;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.pampasim.core.Simulation;
 import org.pampasim.core.events.*;
 import org.pampasim.events.Memory.*;
 import org.pampasim.core.entity.AbstractSimEntity;
 import org.pampasim.memory.resources.PageTableEntry;
 import org.pampasim.memory.resources.ProcessPageTable;
+import org.pampasim.resources.Process;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +16,9 @@ import java.util.Set;
 
 public class PageTableManager extends AbstractSimEntity {
 
-    private final Map<Long, Map<Long, ProcessPageTable>> pageTableMap;
+    private final Logger LOGGER = LogManager.getLogger(PageTableManager.class);
+    private final Map<Long, ProcessPageTable> pageTableMap;
+
 
     public PageTableManager(Simulation simulation) {
         super(simulation);
@@ -39,16 +44,22 @@ public class PageTableManager extends AbstractSimEntity {
     }
 
     private void handleMemoryAllocate(Allocate event) {
-        //TODO: Stub
+        Process process = event.getProcess();
+        ProcessPageTable pageTable = new ProcessPageTable(process.getSize());
+        pageTableMap.put(process.getPid().getId(), pageTable);
+        LOGGER.debug("Processo de ID {} : Entrada na tabela da páginas criada com sucesso", process.getPid().toString());
         scheduleToNextClock(new AllocateFinished(this, event.getProcess()));
     }
 
     private void handleMemoryDeletePageTableEntry(DeletePageTableEntry event) {
-        //TODO: Stub
+        Process process = event.getProcess();
+        pageTableMap.remove(process.getPid().getId());
+        LOGGER.debug("Processo de ID {} : Entrada na tabela da páginas removida com sucesso", process.getPid().toString());
         scheduleToNextClock(new FreeProcessMemory(this, event.getProcess()));
     }
 
     private void handleMemoryTlbNoTranslation(TlbNoTranslation event) {
+        //TODO: the process will store which of it's pages its trying to access, that info is needed for this to work
         //TODO: can result in a MemoryPageHit or a MemoryPageFault event
         //TODO: Stub
         scheduleToNextClock(new PageHit(this, event.getProcess()));
