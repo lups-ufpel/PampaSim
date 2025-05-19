788b2e7b1e66e70a0814960a0b6e8e4eaa38978e
Pedro <pedrosouza7d4@gmail.com>
2025-05-19 02:29:07 -0300

    tweak dsl data model

--- core/src/main/java/org/pampasim/core/dsl/metadata/Event.java
+++ core/src/main/java/org/pampasim/core/dsl/metadata/Event.java
@@ -6,16 +6,16 @@ import lombok.Getter;
 @EqualsAndHashCode
 @Getter
 public class Event {
-    String name;
-    Class<?> dataClass;
-    boolean realtime;
-    public Event(String name, Class<?> dataClass, boolean realtime) {
+    final String name;
+    final String groupName;
+    final boolean realtime;
+    public Event(String name, EventGroup group, boolean realtime) {
         this.name = name;
-        this.dataClass = dataClass;
+        this.groupName = group.name();
         this.realtime = realtime;
     }
     @Override
     public String toString() {
-        return "Event " + getName() + ((getDataClass() != null)? " transmitting " + getDataClass() : "");
+        return "Event " + getName() + " of group " + groupName;
     }
 }
