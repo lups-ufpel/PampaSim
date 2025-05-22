b877e47b64c39ac0c8726ba0b74ad3974123d4b3
Jan Soares <jansoares@inf.ufpel.edu.br>
2025-05-21 21:11:47 -0300

    Cleaning unused imports

--- sim/src/main/java/org/pampasim/DialogService.java
+++ sim/src/main/java/org/pampasim/DialogService.java
@@ -1,8 +1,6 @@
 package org.pampasim;
 
-import javax.swing.text.html.Option;
-import java.util.List;
-import java.util.Objects;
+
 import java.util.Optional;
 
 public interface DialogService <T> {
