d5c2240fcc68eda1dc42390b131dff6a3a2d97f5
Pedro <pedrosouza7d4@gmail.com>
2025-05-26 09:49:03 -0300

    fix events module-info, make it generated

--- events/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.pampasim.events {
-    requires org.pampasim.core;
-    requires org.pampasim.resources;
-    requires lombok;
-    exports org.pampasim.events;
-    exports org.pampasim.events.Process;
-    exports org.pampasim.events.External;
-    exports org.pampasim.events.Memory;
-}
