bad9cb1011b7c7e41904674fa1a97861ca9acb38
IgorDutra <igorgomesdutra@gmail.com>
2025-07-23 18:11:02 -0300

    added skeleton of page substitution algorithm classes

---
+++ memory/src/main/java/org/pampasim/memory/entity/algorithms/FIFO.java
@@ -0,0 +1,12 @@
+package org.pampasim.memory.entity.algorithms;
+
+import org.pampasim.resources.memory.PageTableEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FIFO implements PageReplacementAlgorithm{
+    public ArrayList<PageTableEntry> pickPagesToSwap(int quantity, List<PageTableEntry> pageTableEntries) {
+        return null;
+    }
+}
---
+++ memory/src/main/java/org/pampasim/memory/entity/algorithms/LFU.java
@@ -0,0 +1,12 @@
+package org.pampasim.memory.entity.algorithms;
+
+import org.pampasim.resources.memory.PageTableEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LFU implements PageReplacementAlgorithm{
+    public ArrayList<PageTableEntry> pickPagesToSwap(int quantity, List<PageTableEntry> pageTableEntries) {
+        return null;
+    }
+}
